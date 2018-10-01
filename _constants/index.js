3cf53bf70a2d783cc7206664b311991a46ac5445
askmyselfwhy <romanefimov2@gmail.com>
2018-10-02 00:08:50 +0300

    1

---
+++ _constants/index.js
@@ -0,0 +1,6 @@
+let messageConstants = require('./message.constants')
+
+module.exports = {
+  API_SECRET: 'ilovealyona15031996VERYMUCH',
+  ...messageConstants
+}
---
+++ _constants/message.constants.js
@@ -0,0 +1,7 @@
+module.exports = {
+  NO_ACCESS: "You have no permission for such operation!",
+  WRONG_QUERY: "Something is wrong with",
+  WRONG_CREDENTIALS: "Your email or password is wrong",
+  USER_EXIST: "User with such an email already exists!",
+  DB_ERROR: "There are some troubles with on server!",
+}
